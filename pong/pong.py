84cbd7801f14d3d936a2fa026c580c94f4dc0e2e
arpita-abrol <arpita-abrol@users.noreply.github.com>
2016-07-25 09:40:46 -0400

    made overview, began work on PONG
    overview: controls [pins] added, basic intro to PONG made
    PONG: programming begun

---
+++ pong/pong.py
@@ -0,0 +1,36 @@
+#Arpita Abrol
+#Pong game for Raspberry Pi in Python using RPi.GPIO
+
+###pins used
+#LEFT: GPIO 
+#RIGHT:
+#UP: n/a
+#DOWN: n/a
+
+import pygame, sys
+from pygame.locals import *
+import RPi.GPIO as GPIO
+import time
+import random
+
+#Frames per second
+FPS = 200
+
+#Global Variables
+WIDTH = 400
+HEIGHT = 300
+THICK = 10
+PADDLESIDE = 50
+
+#GPIO SETUP
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(
+
+#Methods
+
+#sets up the window for the game
+def setup():
+    background.fill(0,75,150)
+
+
+setup()
